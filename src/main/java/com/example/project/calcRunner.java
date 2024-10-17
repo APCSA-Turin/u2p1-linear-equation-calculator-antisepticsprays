ea5a87135217cc4bdbf07e962ce0027db951dd16
antisepticsprays <andrewc509@nycstudents.net>
2024-10-17 13:15:53 +0000

    i like fishies

---
+++ src/main/java/com/example/project/calcRunner.java
@@ -0,0 +1,8 @@
+package com.example.project;
+
+public class calcRunner {
+    public static void main(String[] args) {
+        LinearCalculator c = new LinearCalculator("(10,5)","(6,-1)");
+        System.out.println(c.printInfo());
+    }
+}
